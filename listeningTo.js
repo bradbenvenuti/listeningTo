a4838ebcd76dd5f10c15745b5599caa1340a0068
Brad Benvenuti <bradbenvenuti@gmail.com>
2012-02-09 17:57:43 -0500

    ran through jshint and fixed

--- listeningTo.js
+++ listeningTo.js
@@ -12,30 +12,31 @@
             
             var elem = this;
             //lastFM address
-            var apiUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" 
-                            + username + "&api_key=" + apiKey + "&nowplaying=true&format=json&callback=?";
+            var apiUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + username + "&api_key=" + apiKey + "&nowplaying=true&format=json&callback=?";
             
 
             function formateThisDate(time)
             {       
                     //Convert to user's timezone
-                    var dateNow = new Date();
-                    var localOffset = -dateNow.getTimezoneOffset();
-                    var adjustedDate = new Date(time);
-                    var adjustedTimeToShow;
-                    var hours;
-                    var minutes;
-                    var minutesText;
-                    var hoursText;
+                    var dateNow = new Date(),
+                    localOffset = -dateNow.getTimezoneOffset(),
+                    adjustedDate = new Date(time),
+                    adjustedTimeToShow,
+                    hours,
+                    hoursText,
+                    minutes,
+                    minutesText,
+                    sText;
+
                     adjustedDate.setMinutes(adjustedDate.getMinutes() + localOffset);
                     
-                    adjustedDate = Math.round(parseInt(dateNow.getTime() - adjustedDate.getTime())/ 60000);
+                    adjustedDate = Math.round(parseInt(dateNow.getTime() - adjustedDate.getTime(), 10)/ 60000);
 
                     if (adjustedDate >= 60)
                     {
                         hours = Math.floor(adjustedDate/60);
                         minutes =  adjustedDate % 60;
-                        hoursText = hours === 1 ? " hour" : " hours";
+                        hoursText = hours === 1 ? " hour " : " hours ";
                         minutesText = minutes === 1 ? " minute" : " minutes";
 
                         adjustedTimeToShow = hours + hoursText + minutes + minutesText;
@@ -49,18 +50,17 @@
                     return adjustedTimeToShow;
             }
 
-            console.log(apiUrl);
+            //console.log(apiUrl);
             $.getJSON(apiUrl, function(data) {
-                var items = [];
-                
-                var count = settings.showCount;
-                var feed = data.recenttracks.track;
-                var timeToShow;
+                var items = [],
+                count = settings.showCount,
+                feed = data.recenttracks.track,
+                timeToShow;
                 
                 for (var i=0; i<count; i++){
 
-                  var timeToShow = feed[i].date === undefined ? "Now Playing" : feed[i].date['#text'];
-                  if (timeToShow != "Now Playing")
+                  timeToShow = feed[i].date === undefined ? "Now Playing" : feed[i].date['#text'];
+                  if (timeToShow !== "Now Playing")
                   {
                      timeToShow = formateThisDate(timeToShow) + ' ago';
                   }
@@ -68,7 +68,7 @@
                   
 
                   items.push('<li>' + feed[i].artist['#text'] + ' - ' + feed[i].name  + ' - ' + timeToShow + '</li>');
-                };
+                }
 
                 $('<ul/>', {
                   'class': 'listeningTo',
